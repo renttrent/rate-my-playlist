c352cbe58d57cba587c88664f4c5a34035b420c4
rei <reiballa@gmail.com>
2022-03-18 11:13:41 +0100

    still not workin

--- pages/welcome/index.tsx
+++ pages/welcome/index.tsx
@@ -1,9 +1,19 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 import { GetServerSideProps, NextPage } from "next";
 import { getProviders, SessionProviderProps, signIn, useSession } from "next-auth/react";
 import Router from "next/router";
 import { useEffect } from "react";
-import { LogInButton } from "../../components/LogInButton";
+
+
+const LogInButton: React.FC<{signIn: Function, providerId: string, providerCallback: string}> = ({signIn, providerId, providerCallback}) => {
+  return (
+    <Button bg="green.300" _hover={{ bg: "green.500" }}
+      onClick={() => signIn(providerId, { callbackUrl: providerCallback})}
+    >
+      Login with Spotify
+    </Button>
+  )
+}
 
 const Welcome: NextPage<{providers: SessionProviderProps}> = ({providers}) => {
   const { data: session } = useSession()
